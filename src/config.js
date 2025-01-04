ceb3505267b3ca8a25ae0e468ff85e0c48137869
shalini <s.rathore43@gmail.com>
2025-01-04 20:45:34 +0530

    kjbk

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_URL="";
+export const API_URL="https://backendfinal-bephn28r2-shalini210s-projects.vercel.app/";
+// https://backendfinal-bephn28r2-shalini210s-projects.vercel.app/api/contacts
