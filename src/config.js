ad314934a337b30bd2a8e3b4d0dfb1a7b96198e6
shalini <s.rathore43@gmail.com>
2025-01-04 21:10:15 +0530

    front test

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL="https://backendfinal-bephn28r2-shalini210s-projects.vercel.app";
+export const API_URL="https://backend-ab2y-llvmi03b0-shalini210s-projects.vercel.app";
 // https://backendfinal-bephn28r2-shalini210s-projects.vercel.app/api/contacts
