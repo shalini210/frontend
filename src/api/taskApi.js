5795bea381d4ef9c0d2276828e495100be4cbb0e
shalini <s.rathore43@gmail.com>
2025-01-04 12:49:09 +0530

    dsa

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/tasks';
+const API_URL = 'https://backend-ab2y-ikazrn0vs-shalini210s-projects.vercel.app/api/tasks';
 
 export const getTasks = async () => {
   const response = await axios.get(API_URL);
